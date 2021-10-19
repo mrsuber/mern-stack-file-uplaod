fedee685f43f0ad19581e6a48fbab291eb3f344b
mohamad <msiysinyuy@gmail.com>
2021-10-19 14:19:19 +0100

    both single and multiple file upload back end completed

--- routes/file-upload-routes.js
+++ routes/file-upload-routes.js
@@ -2,9 +2,12 @@
 
 const express = require('express')
 const {upload} = require('../helpers/filehelper')
-const {singleFileUpload} = require('../controllers/fileuploaderController')
+const {singleFileUpload,multipleFileUpload,getallSingleFiles,getallMultipleFiles} = require('../controllers/fileuploaderController')
 const router = express.Router();
 
 router.post('/singleFile',upload.single('file'),singleFileUpload);
+router.post('/multipleFiles',upload.array('files'),multipleFileUpload);
+router.get('/getallSingleFiles',getallSingleFiles)
+router.get('/getallMultipleFiles',getallMultipleFiles)
 
 module.exports = {routes:router}
